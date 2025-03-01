3cdfe27c2fcec0ca56847441fcb8798fbcb6c53e
Karthi~Natarajan <136811565+Karthi-Natarajan@users.noreply.github.com>
2025-03-01 13:01:52 +0530

    Add files via upload

---
+++ To find First_digit and Last_digit.py
@@ -0,0 +1,21 @@
+# To  print First_digit and Last_digit
+
+num = input("Enter a 3 digit number: ")
+num_value = 0
+for digit in num :
+    num_value = num_value*10 + (ord(digit) - ord('0'))
+
+last_digit = num_value
+while last_digit >= 10:
+    last_digit -= 10
+
+first_digit = num_value
+while first_digit >= 10:
+    first_digit //= 10
+
+print(first_digit,last_digit)
+
+#In Build-in function
+
+num = int(input("Enter a 3 digit number: "))
+print ( num % 10 , num // 100 )
