93c4f83d97462d137fc274d83b2982e24ee84a05
Karthi~Natarajan <136811565+Karthi-Natarajan@users.noreply.github.com>
2025-03-01 12:10:43 +0530

    Add files via upload

---
+++ Percentage of marks.py
@@ -0,0 +1,10 @@
+marks=[]
+i=0
+while i < 5:
+    marks.append(int(input("enter the num {i+1} :")))
+    i+=1
+total = 0
+for mark in marks:
+    total+=mark
+percentage = total / 5
+print(f"Percentage : {percentage :.2f}%")
