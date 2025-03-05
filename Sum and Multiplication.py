18e97f21f2785b63e2dea12fb44e810b5dbc2b9b
Karthi~Natarajan <136811565+Karthi-Natarajan@users.noreply.github.com>
2025-03-05 13:08:50 +0530

    Add files via upload

---
+++ Sum and Multiplication.py
@@ -0,0 +1,9 @@
+#Sum and Multiplication
+
+a=int(input("Enter the number : "))
+b=int(input("Enter the number : "))
+c=int(input("Enter the number : "))
+
+result = (a+b)*c
+
+print("The answer is : ",result)
